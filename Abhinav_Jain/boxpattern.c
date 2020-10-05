4632dc371c51958253094dde759d96fd69874188
Aabhinavjain <72297124+Aabhinavjain@users.noreply.github.com>
2020-10-05 19:31:16 +0530

    Create boxpattern.c

---
+++ Abhinav_Jain/boxpattern.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int
+main ()
+{
+  int i, j, n;
+  printf ("Enter n: ");
+  scanf ("%d", &n);
+  for (i = 0; i <= n; i++)
+    {
+      for (j = 1; j <= n; j++)
+      {
+	  printf ("-");
+	}
+      printf ("\n");
+    }
+  return 0;
+}
