f379ac98f01ae62101a75f2f654d6d8e1a547378
shrutikathakur007 <72661982+shrutikathakur007@users.noreply.github.com>
2020-10-10 22:33:02 +0530

    Create pattern.c

---
+++ shrutika/pattern.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+   int i,j,x=1;
+for(i=1;i<=4;i++)
+{
+    for(j=1;j<=i;j++)
+    {
+        printf("%d ",x);
+        x++;
+    }
+    printf("\n");
+}
+    return 0;
+}
